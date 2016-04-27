a0362f3e6f28dad501c088467d1196215476cd8d
bigOnce <hnhatran2008@gmail.com>
2016-04-28 02:07:23 +0700

    add tablet page commit n2

--- Futbol/MVC/UI/Pages/RootPage.cs
+++ Futbol/MVC/UI/Pages/RootPage.cs
@@ -15,13 +15,10 @@ namespace Futbol
 			var FirstPage = new HomePage ();
 //			FirstPage.Icon = "icon.png";
 			FirstPage.Title = ResourceLoader.LocalizeString ("string_tab_video", "");
-			FirstPage.Title = "aaaaaa";
 
 			var SecondPage = new GeneralPage ();
 //			SecondPage.Icon = "icon.png";
 			SecondPage.Title = ResourceLoader.LocalizeString ("string_tab_livescore", "");
-//
-			SecondPage.Title = "bbb";
 
 			Children.Add (FirstPage);
 			Children.Add (SecondPage);
